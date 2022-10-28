ca7711cc82a28878a0eb0391223b919c1960cc49
Myroslav Tovtyk <tovmyr@gmail.com>
2022-10-28 10:06:34 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
-import Feedback from './Feedback/Feedback';
+import Controls from './FeedbackControls/FeedbackControls';
+import Section from './Statistics/Statistics';
+import Value from './Value/Value';
+import Notification from './Notification/Notification';
+import { Container } from '../components/Global.styled';
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleOptions = feedback => {
+    this.setState(prev => {
+      return {
+        [feedback]: prev[feedback] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const Total = this.state.good + this.state.neutral + this.state.bad;
+    return Total;
+  };
+  countPositiveFeedbackPercentage = () => {
+    if (this.countTotalFeedback() === 0) {
+      return;
+    } else {
+      const percentPositive = Math.round(
+        (this.state.good / this.countTotalFeedback()) * 100
+      );
+
+      return percentPositive;
+    }
+  };
+
   render() {
+    const { good, neutral, bad } = this.state;
     return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 24,
-          color: '#010101',
-        }}
-      >
-        <Feedback />
-      </div>
+      <Container>
+        <Controls
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.handleOptions}
+        />
+
+        <Section title="Statistics:">
+          {this.countTotalFeedback() ? (
+            <Value
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positive={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="No feedbacks yet:(" />
+          )}
+        </Section>
+      </Container>
     );
   }
 }
+
 export default App;
